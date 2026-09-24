553d34e5aaad94cb29ae5075a499a7cbdbbfa485
aphmoreau-ctrl <aph.moreau@gmail.com>
2026-09-24 13:52:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Aéroclub 70 : fonctionnement hors ligne */
-const CACHE='ac70-v5';
+const CACHE='ac70-v6';
 const CORE=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
